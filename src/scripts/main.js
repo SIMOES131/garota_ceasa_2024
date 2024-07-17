9e992033316a8a244eefea769360f23c0332cd4a
Simoes Oliveira <cammom131@gmail.com>
2024-07-17 17:57:43 -0300

    primeiro commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 25, 2024 19:00:00");
+const dataDoEvento = new Date("Oct 31, 2024 20:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
